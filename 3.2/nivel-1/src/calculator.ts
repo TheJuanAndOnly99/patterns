0cdcb0792037542428231b8d271b69e82424f4f8
Juan Estrella <juan.estrella@finos.org>
2023-05-30 10:13:40 +0200

    add throw Error

--- 3.2/nivel-1/src/calculator.ts
+++ 3.2/nivel-1/src/calculator.ts
@@ -1,57 +1,57 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import Middlewares from './middleware'
+import Middlewares from './middleware';
 
 interface Calculation {
-  operation: string;
-  operands: number[];
+	operation: string;
+	operands: number[];
 }
 
 interface InputData {
-  operations: Calculation[];
+	operations: Calculation[];
 }
 
 // Read the JSON file
-const jsonFilePath = path.resolve(__dirname, '../static/input.json');
+const jsonFilePath = path.resolve(__dirname, './input.json');
 const jsonInput: string = fs.readFileSync(jsonFilePath, 'utf8');
 const inputData: InputData = JSON.parse(jsonInput);
 
 const performCalculation = (calc: Calculation): number => {
-  const { operation, operands } = calc;
-  let result: number;
-  const middlewares = Middlewares.getInstance();
+	const { operation, operands } = calc;
+	let result: number;
+	const middlewares = Middlewares.getInstance();
 
-  // Log the operation
-  console.log(`Performing operation: '${operation}' on ${operands}`);
+	// Log the operation
+	console.log(`Performing operation: '${operation}' on ${operands}`);
 
-  // Apply the middlewares in sequence
-  const middlewareNames = Object.keys(middlewares.getMiddlewares());
-  middlewareNames.forEach((middlewareName) => {
-    middlewares.applyMiddleware(middlewareName, operands);
-    // Log the new operands
-    console.log(`New operands: ${operands}`);
-  });
-    
-  switch (operation) {
-    case 'add':
-      result = operands.reduce((acc, curr) => acc + curr);
-      break;
-    case 'subtract':
-      result = operands.reduce((acc, curr) => acc - curr);
-      break;
-    case 'multiply':
-      result = operands.reduce((acc, curr) => acc * curr);
-      break;
-    default:
-      throw new Error('Invalid operation');
-  }
+	// Apply the middlewares in sequence
+	const middlewareNames = Object.keys(middlewares.getMiddlewares());
+	middlewareNames.forEach((middlewareName) => {
+		middlewares.applyMiddleware(middlewareName, operands);
+		// Log the new operands
+		console.log(`New operands: ${operands}`);
+	});
 
-  return result;
+	switch (operation) {
+		case 'add':
+			result = operands.reduce((acc, curr) => acc + curr);
+			break;
+		case 'subtract':
+			result = operands.reduce((acc, curr) => acc - curr);
+			break;
+		case 'multiply':
+			result = operands.reduce((acc, curr) => acc * curr);
+			break;
+		default:
+			throw new Error('Invalid operation');
+	}
+
+	return result;
 };
 
 inputData.operations.forEach((calculation) => {
-  const result = performCalculation(calculation);
-  console.log(`Result (${calculation.operation}): ${result}`);
+	const result = performCalculation(calculation);
+	console.log(`Result (${calculation.operation}): ${result}`);
 });
 
 export default performCalculation;
